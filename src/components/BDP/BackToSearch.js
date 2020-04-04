3f81e737555ccb852ee851cf66982190cd8b4df2
Aaron Vanston <avanston@seek.com.au>
2020-04-04 16:32:05 +1100

    feat: add icons

--- src/components/BDP/BackToSearch.js
+++ src/components/BDP/BackToSearch.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { FiChevronLeft } from 'react-icons/fi'
 
 import Container from '../shared/Container'
 
@@ -9,20 +10,36 @@ const BackBar = styled.div`
   align-items: center;
 `
 
+const Chevron = styled.span`
+  color: ${({ theme }) => theme.colour.black};
+  display: inline-flex;
+  padding-right: 0.25rem;
+`
+
 const BackLink = styled.a`
   color: ${({ theme }) => theme.colour.greyLight};
   text-decoration: none;
-`
+  display: flex;
+  align-items: center;
 
-const Chevron = styled.span`
-  color: ${({ theme }) => theme.colour.black};
+  &:hover {
+    text-decoration: none;
+    color: ${({ theme }) => theme.colour.primary};
+
+    & > ${Chevron} {
+      color: ${({ theme }) => theme.colour.primary};
+    }
+  }
 `
 
 const BackToSearch = () => (
   <BackBar>
     <Container>
       <BackLink onClick={() => window.history.back()}>
-        <Chevron>&lt;</Chevron> Back to results
+        <Chevron>
+          <FiChevronLeft size="1.5rem" />
+        </Chevron>{' '}
+        Back to results
       </BackLink>
     </Container>
   </BackBar>
