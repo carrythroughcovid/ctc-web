0f95a5da4a5b8a9407880a8e2b8de7f1f118dea6
Aaron Vanston <avanston@seek.com.au>
2020-04-04 22:23:07 +1100

    feat: custom 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'gatsby'
+
+import Page from '../components/shared/Page'
+import Container from '../components/shared/Container'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  <Page>
+    <Container>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>
+        Head back to our <Link to="/search">search page here.</Link>
+      </p>
+    </Container>
+  </Page>
 )
 
 export default NotFoundPage
