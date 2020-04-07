5fdaeecec7c60ac79ead396e0dd15dc599ce57e4
Matt Perrott <matt.perrott@rea-group.com>
2020-04-07 15:54:08 +1000

    create signupFields file

---
+++ src/components/signup/signupFields.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import TextFormField from './TextFormField'
+
+export const signupFields = {
+  owner_name: (
+    <TextFormField
+      name="owner_name"
+      label="Your Name"
+      placeholder="Full Name"
+    />
+  ),
+}
