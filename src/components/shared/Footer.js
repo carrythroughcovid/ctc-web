128ab4ed30a835fa9e8685bd27cfabba116b13de
Jon Meyers <jon.meyers@rea-group.com>
2020-04-08 15:04:14 +1000

    fix: add to for signup button in footer

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -101,7 +101,7 @@ const Footer = () => {
             <FooterLink to="/about">About us</FooterLink>
             <FooterLink to="/#explore">Explore offerings</FooterLink>
             <FooterLink to="/terms">Terms</FooterLink>
-            <CTALink>
+            <CTALink to="/signup">
               Sign up now <ArrowIcon size="1.5rem" />
             </CTALink>
           </div>
