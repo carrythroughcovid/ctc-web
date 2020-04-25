feae9a1ed8f6ed994064f845dfbba67a002cf591
Aaron Vanston <avanston@seek.com.au>
2020-04-25 18:13:45 +1000

    feat: add small logo for mobile

--- src/components/shared/Logo.js
+++ src/components/shared/Logo.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import media from '../../utils/media'
 import LogoDark from '../../assets/logoDark.svg'
 import LogoLight from '../../assets/logoLight.svg'
+import LogoLightSmall from '../../assets/LogoLightSmall.svg'
 
-const StyledLogo = styled.div`
-  font-size: 28px;
-  line-height: 1;
-  color: black;
+const DesktopLogo = styled.div`
+  display: none;
+
+  ${media.sm`
+    display: block;
+  `}
+`
+const MobileLogo = styled.div`
+  display: block;
+
+  ${media.sm`
+    display: none;
+  `}
 `
 
-const Logo = ({ light }) => {
-  return <StyledLogo>{light ? <LogoDark /> : <LogoLight />}</StyledLogo>
-}
+const Logo = ({ light }) => (
+  <>
+    <DesktopLogo>{light ? <LogoDark /> : <LogoLight />}</DesktopLogo>
+    <MobileLogo>
+      <LogoLightSmall />
+    </MobileLogo>
+  </>
+)
 
 export default Logo
